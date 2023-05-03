3f1db164138db78ee882b3ca386ee2437b50d24b
Shardul_Joshi <81522444+Shardul2002@users.noreply.github.com>
2023-05-03 22:33:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <h3>
-          React App Hosted Sucessfully 
+          React App Hosted Successfully 
         </h3>
       </header>
     </div>
